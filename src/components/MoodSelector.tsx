64d8099223fae340ee81402fa4b7cba4e9d12c63
tk-206 <taekyoung206@gmail.com>
2025-11-13 23:02:25 +0900

    기능 구현

--- src/components/MoodSelector.tsx
+++ src/components/MoodSelector.tsx
@@ -1,1 +1,23 @@
-export default MoodSelector
+import React from "react"
+
+interface MoodSelectorProps {
+  onSelect: (mood: string) => void
+}
+
+const moods = ["😄", "🙂", "😐", "😔", "😭"]
+
+export default function MoodSelector({ onSelect }: MoodSelectorProps) {
+  return (
+    <div className="flex gap-3 justify-center mt-4">
+      {moods.map((mood) => (
+        <button
+          key={mood}
+          onClick={() => onSelect(mood)}
+          className="text-3xl hover:scale-125 transition-transform"
+        >
+          {mood}
+        </button>
+      ))}
+    </div>
+  )
+}
